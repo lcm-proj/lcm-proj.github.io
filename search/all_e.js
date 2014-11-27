7f6e80206dd956d0d3112ce1cfcbeb336e664425
Albert Huang <ashuang@gmail.com>
2014-11-26 19:56:52 -0800

    documentation updates

--- search/all_e.js
+++ search/all_e.js
@@ -3,5 +3,5 @@ var searchData=
   ['the_20lua_20lcm_20api',['The Lua LCM API',['../lua_api.html',1,'']]],
   ['timestamp',['timestamp',['../struct__lcm__eventlog__event__t.html#a59d7d75e00f753352679fd2ed61f1357',1,'_lcm_eventlog_event_t::timestamp()'],['../structlcm_1_1LogEvent.html#ab9e0034b6ea783c9ec77768adb20d751',1,'lcm::LogEvent::timestamp()']]],
   ['todo_20list',['Todo List',['../todo.html',1,'']]],
-  ['tutorial',['Tutorial',['../tutorial_general.html',1,'']]]
+  ['tutorial_20and_20examples',['Tutorial and examples',['../tutorial_general.html',1,'']]]
 ];
--- search/pages_7.js
+++ search/pages_7.js
@@ -2,5 +2,5 @@ var searchData=
 [
   ['the_20lua_20lcm_20api',['The Lua LCM API',['../lua_api.html',1,'']]],
   ['todo_20list',['Todo List',['../todo.html',1,'']]],
-  ['tutorial',['Tutorial',['../tutorial_general.html',1,'']]]
+  ['tutorial_20and_20examples',['Tutorial and examples',['../tutorial_general.html',1,'']]]
 ];
